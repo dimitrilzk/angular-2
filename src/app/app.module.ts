c08caf9582ce1f176340edb0ad5a981b3da02d45
dimitrilzk <dimitrilazuka95@gmail.com>
2022-11-02 15:00:16 +0100

    starter pack

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'page1'
+  },
+  {
+    path: 'page1',
+    component: Page1Component
+  },
+  {
+    path: 'page2',
+    component: Page2Component
+  },
+  {
+    path: 'page3',
+    component: Page3Component
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
